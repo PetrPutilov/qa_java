7c35a34b303536451c33abbaef4376bab7e88c95
PetrPutilov <putilov_11@icloud.com>
2024-06-26 15:19:09 +0300

    correct CatTest

--- src/test/java/CatTest.java
+++ src/test/java/CatTest.java
@@ -3,8 +3,8 @@ import com.example.Feline;
 import org.junit.Test;
 import org.junit.jupiter.api.Assertions;
 import org.junit.runner.RunWith;
+import org.mockito.Mock;
 import org.mockito.Mockito;
-import org.mockito.Spy;
 import org.mockito.junit.MockitoJUnitRunner;
 
 import java.util.List;
@@ -12,20 +12,21 @@ import java.util.List;
 @RunWith(MockitoJUnitRunner.class)
 public class CatTest {
 
-    @Spy
+    @Mock
     private Feline feline;
 
     @Test
     public void testGetFood() throws Exception {
         Cat cat = new Cat(feline);
+        Mockito.when(feline.eatMeat()).thenReturn(List.of("Животные", "Птицы", "Рыба"));
         List<String> catFood = cat.getFood();
-        Mockito.verify(feline, Mockito.times(1)).getFood("Хищник");
+        Mockito.verify(feline, Mockito.times(1)).eatMeat();
         Assertions.assertEquals(List.of("Животные", "Птицы", "Рыба"), catFood);
     }
 
     @Test
     public void testGetSound() {
-        Cat cat = new Cat(new Feline());
+        Cat cat = new Cat(feline);
         String sound = cat.getSound();
         Assertions.assertEquals("Мяу", sound);
     }
